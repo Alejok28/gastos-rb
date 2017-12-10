93b5661bdf97813bb923ba906b2f7638e25ac84d
Alejok28 <alejo_k28@hotmail.com>
2017-12-10 12:53:31 -0500

    Adds the scope to search form

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,9 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC").concepto("%#{params[:concept]}%").id("%#{params[:category_id]}%")
+    # @expenses = Expense.where("concept like ?", "%#{params[:concept]}%")
+    # @expenses = Expense.where("category_id = ? ", params[:category_id])
+
+
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ActiveRecord::Base
   belongs_to :category
+  scope :concepto, ->(c) { where("concept like ?", c) }
+  scope :id,       ->(id) { where("category_id like ?", id) }
 end
